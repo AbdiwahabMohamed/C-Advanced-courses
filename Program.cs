119047bc22651109191458d239d81688bd2c6b6c
Abdiwahab mohamed <fahadfar69@gmail.com>
2024-01-01 14:37:29 +0300

    c# Advanced course

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace LearningCSharp
+{
+public class Person 
+ {
+ public string firstName;
+ public string lastName;
+
+ public void GreetPerson()
+ {
+    Console.WriteLine($"Welcome to {this.firstName} {this.lastName}");
+ }
+ }
+}
+
+namespace  LearningCSharp
+{
+    public class Calculate 
+    {
+
+    }
+}
